4c15178b2976dc0314445a3e73db44ea1185f022
misa.shikama <misaruby0303@gmail.com>
2021-06-07 15:37:02 +0000

    ninth

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
   end
   
   def show
-    @task = Task.find(params[:id])
+    set_task
   end
   
   def new
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    set_task
     
     if @task.save
       flash[:success] = "タスクが正常に追加されました"
@@ -25,11 +25,11 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+    set_task
   end
   
   def update
-    @task = Task.find(params[:id])
+    set_task
     
     if @task.update(task_params)
       flash[:success] = "タスクは正常に更新されました"
@@ -43,7 +43,7 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
+    set_task
     @task.destroy
     
     flash[:success] = "タスクは正常に削除されました"
@@ -54,8 +54,11 @@ class TasksController < ApplicationController
   
   #Strong Parameter
   
+  def set_task
+    @task = Task.find(params[:id])
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
 end
