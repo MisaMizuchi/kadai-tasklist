641556e7af0b25caff511c6c54280bfec2d15042
misa.shikama <misaruby0303@gmail.com>
2021-06-08 06:47:31 +0000

    eleventh

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,8 @@ class TasksController < ApplicationController
   end
   
   def create
-    set_task
+    
+    @task = Task.new(task_params)
     
     if @task.save
       flash[:success] = "タスクが正常に追加されました"
@@ -52,10 +53,10 @@ class TasksController < ApplicationController
   
   private
   
-  #Strong Parameter
   
   def set_task
     @task = Task.find(params[:id])
+  end
   
   def task_params
     params.require(:task).permit(:content, :status)
